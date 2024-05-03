c0f3f0d80b0843c164421960f13df49842fbcc9e
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-05-04 01:12:52 +0700

    revison in edit.blade

--- resources/views/category/edit.blade.php
+++ resources/views/category/edit.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-semibold leading-tight text-gray-800 dark:text-gray-200">
-            {{ __('Todo') }}
+            {{ __('Category') }}
         </h2>
     </x-slot>
 
@@ -9,18 +9,18 @@
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <form method="post" action="{{ route('todo.update', $todo) }}" class="">
+                    <form method="post" action="{{ route('category.update', $todo) }}" class="">
                         @csrf
                         @method('patch')
                         <div class="mb-6">
                             <x-input-label for="title" :value="__('Title')" />
                             <x-text-input id="title" name="title" type="text" class="block w-full mt-1"
-                                :value="old('name', $todo->title)" required autofocus autocomplete="title" />
+                                :value="old('name', $category->title)" required autofocus autocomplete="title" />
                             <x-input-error class="mt-2":messages="$errors->get('title')" />
                         </div>
                         <div class="flex items-center gap-4">
                             <x-primary-button>{{ __('Save') }}</x-primary-button>
-                            <x-cancel-button href="{{ route('todo.index') }}" />
+                            <x-cancel-button href="{{ route('category.index') }}" />
                         </div>
                     </form>
                 </div>
