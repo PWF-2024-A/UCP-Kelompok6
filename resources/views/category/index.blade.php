18e68675e5ceb69ac1732aae3a8da615ba4f8af5
Mochamad Aldi Raihan Fachrizal <onlymarfa69@gmail.com>
2024-05-04 00:31:47 +0700

    Add (tbody) dynamic todo list rendering and actions

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -44,8 +44,78 @@
                                 </th>
                             </tr>
                         </thead>
+                        <tbody>
+                            @forelse ($todos as $todo)
+                                <tr class="odd:bg-white odd:dark:bg-gray-800 even:bg-gray-50 even:dark:bg-gray-700">
+
+                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+
+                                        <a href="{{ route('todo.edit', $todo) }}"
+                                            class="hover:underline">{{ $todo->title }}</a>
+
+                                    </td>
+
+                                    <td class="hidden px-6 py-4 md:block">
+
+                                        @if ($todo->is_complete == false)
+                                            <span
+                                                class="bg-blue-100 text-blue-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-blue-900 dark:text-blue-300">
+
+                                                Ongoing
+
+                                            </span>
+                                        @else
+                                            <span
+                                                class="bg-green-100 text-green-800 text-xs font-medium mr-2 px-2.5 py-0.5 rounded dark:bg-green-900 dark:text-green-300">
+
+                                                Completed
+                                            </span>
+                                        @endif
+
+                                    </td>
+
+                                    <td class="px-6 py-4">
+                                        <div class="flex space-x-3">
+                                            {{-- Action Here --}}
+                                            @if ($todo->is_complete == false)
+                                                <form action="{{ route('todo.complete', $todo) }}" method="Post">
+                                                    @csrf
+                                                    @method('PATCH')
+                                                    <button type="submit" class="text-green-600 dark:text-green-400">
+                                                        Complete
+                                                    </button>
+                                                </form>
+                                            @else
+                                                <form action="{{ route('todo.uncomplete', $todo) }}" method="Post">
+                                                    @csrf
+                                                    @method('PATCH')
+                                                    <button type="submit" class="text-blue-600 dark:text-blue-400">
+                                                        Uncomplete
+                                                    </button>
+                                                </form>
+                                            @endif
+                                            <form action="{{ route('todo.destroy', $todo) }}" method="Post">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="text-red-400 dark:text-red-400">
+                                                    Delete
+                                                </button>
+                                            </form>
+                                        </div>
+                                    </td>
+                                </tr>
+                            @empty
+                                <tr class="bg-white dark:bg-gray-800">
+                                    <td scope="row" class="px-6 py-4 font-medium text-gray-900 dark:text-white">
+                                        Empty
+                                    </td>
+                                </tr>
+                            @endforelse
+
+                        </tbody>
                     </table>
                 </div>
+                
             </div>
         </div>
     </div>
