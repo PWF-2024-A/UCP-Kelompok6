ee5aebc5b04b529a7f58ff1e5518433eca9ac62f
ricco-arisdy <114916298+ricco-arisdy@users.noreply.github.com>
2024-05-04 00:18:59 +0700

    Added a section for creating todos with success and danger message handling

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -4,3 +4,31 @@
             {{ __('Todo') }}
         </h2>
     </x-slot>
+
+    <div class="py-12">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm dark:bg-gray-800 sm:rounded-lg">
+                <div class="p-6 text-xl text-gray-900 dark:text-gray-100">
+                    <div class="flex items-center justify-between">
+                        <div>
+                            <x-create-button href="{{ route('todo.create') }}" />
+                        </div>
+                        <div>
+                            @if (session('success'))
+                                <p x-data="{ show: true }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
+                                    class="text-sm text-green-600 dark:text-green-400">{{ session('success') }}
+                                </p>
+                            @endif
+                            @if (session('danger'))
+                                <p x-data="{ show: ture }" x-show="show" x-transition x-init="setTimeout(() => show = false, 5000)"
+                                    class="text-sm text-red-600 dark:text-red-400">{{ session('danger') }}>
+                                </p>
+                            @endif
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</x-app-layout>
