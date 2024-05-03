d26bc2059c5ea84b3e9f213aba47780b36cda70c
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2024-05-03 17:20:32 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\Todo;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+
+        User::create(
+
+            [
+
+                'name' => 'Admin',
+
+                'email' => 'admin@admin.com',
+
+                'email_verified_at' => now(),
+
+                'password' => 'password',
+
+                'remember_token' => Str::random(10),
+
+                'is_admin' => true,
+            ]
+        );
+        User::factory(100)->create();
+        Todo::factory(500)->create();
+    }
+}
