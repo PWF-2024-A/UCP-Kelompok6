d762db4fd3b723209e073f0c022b5c7cb33ea28d
dwiponcosuripto4 <dwiponcosuripto44@gmail.com>
2024-05-04 10:46:05 +0700

    add category controller

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -6,5 +6,52 @@ use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
-    
+    public function index()
+    {
+        $categories = Category::all();
+        return view('category.index', compact('categories'));
+    }
+
+    public function create()
+    {
+        return view('category.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        Category::create([
+            'title' => $request->title,
+            'user_id' => auth()->user()->id,
+        ]);
+
+        return redirect()->route('category.index')->with('success', 'Category created successfully!');
+    }
+
+    public function edit(Category $category)
+    {
+        return view('category.edit', compact('category'));
+    }
+
+    public function update(Request $request, Category $category)
+    {
+        $request->validate([
+            'title' => 'required|max:255',
+        ]);
+
+        $category->update([
+            'title' => $request->title,
+        ]);
+
+        return redirect()->route('category.index')->with('success', 'Category updated successfully!');
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return redirect()->route('category.index')->with('success', 'Category deleted successfully!');
+    }
 }
